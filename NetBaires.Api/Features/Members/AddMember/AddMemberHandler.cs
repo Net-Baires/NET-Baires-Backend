9b3e27d2eb509e51983e186f49157115f63c42c9
Germán <german.kuber@outlook.com>
2019-12-28 09:49:01 -0300

    Update packages

--- NetBaires.Api/Features/Members/AddMember/AddMemberHandler.cs
+++ NetBaires.Api/Features/Members/AddMember/AddMemberHandler.cs
@@ -1,11 +1,9 @@
-﻿using System.Linq;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using AutoMapper;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
 using NetBaires.Api.Features.Members.ViewModels;
 using NetBaires.Api.Helpers;
 using NetBaires.Data;
@@ -29,9 +27,7 @@ namespace NetBaires.Api.Features.Badges.GetBadge
 
         public async Task<IActionResult> Handle(AddMemberCommand request, CancellationToken cancellationToken)
         {
-            var memberToCheck = await _context.Members.FirstOrDefaultAsync(x => (x.Email.ToUpper() == request.Email.ToUpper())
-                                        ||
-                                        (request.MeetupId != null ? x.MeetupId == request.MeetupId : true));
+            var memberToCheck = await _context.Members.FirstOrDefaultAsync(x => (x.Email.ToUpper() == request.Email.ToUpper()));
             if (memberToCheck != null)
                 return HttpResponseCodeHelper.Conflict("Ya se encuentra un usuario registrado con ese email");
 
