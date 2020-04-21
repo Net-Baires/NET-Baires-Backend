8107a4b83b73991302ac7ee87253cdbbe2f2ec3e
Germán Küber <german.kuber@outlook.com>
2020-04-21 18:14:48 -0300

    Add Material

--- NetBaires.Data/Entities/Event.cs
+++ NetBaires.Data/Entities/Event.cs
@@ -185,7 +185,7 @@ namespace NetBaires.Data.Entities
         private static Random random = new Random();
         public static string RandomString(int length)
         {
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
             return new string(Enumerable.Repeat(chars, length)
               .Select(s => s[random.Next(s.Length)]).ToArray());
         }
